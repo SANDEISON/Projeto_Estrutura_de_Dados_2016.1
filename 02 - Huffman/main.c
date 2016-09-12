133bd76be13a394a3f166f597084208e060bd5af
sandeison <ssf@ic.ufal.br>
2016-09-12 00:53:20 -0300

    Criando o modelo do seminario e do Huffman

---
+++ 02 - Huffman/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    //Recebe o Endereço do arquivo
+    char endereco[999];
+    //Variavel para manipular o arquivo
+    FILE *arquivo = NULL;
+
+
+
+
+
+
+    return 0;
+}
